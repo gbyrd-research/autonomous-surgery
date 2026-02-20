a4d0925f44cff29dc81237655984307caf22e68b
gbyrd-research <gbyrd3@jh.edu>
2026-02-20 15:24:32 -0500

    Updating training params

--- autonomous_surgery/dataset/cholecystectomy_dataset.py
+++ autonomous_surgery/dataset/cholecystectomy_dataset.py
@@ -1,6 +1,8 @@
 from typing import List
-from time import time
+import time
 
+import json
+from tqdm import tqdm
 import random
 import torch
 from torch.utils.data import Dataset
@@ -19,53 +21,86 @@ class CholecystectomyDataset(Dataset):
 
     def __init__(
         self,
-        dataset_root: str | Path,
-        split: str = "train",
-        image_keys=None,
+        repo_id: str,
+        root: str | Path,
+        tolerance_s: float,
+        split: str,
+        chunk_size: int = 16,
     ):
         super().__init__()
+        valid_splits = {"train", "val"}
+        assert split in valid_splits, f"Invalid split name, {split}. Must be one of {valid_splits}."
 
-        self.dataset = LeRobotDataset.from_pretrained(dataset_root)
+        # self.split = split
+        # self.chunk_size = chunk_size
 
-        if image_keys is None:
-            image_keys = [
-                "observation.images.endoscope.left",
-                "observation.images.endoscope.right",
-            ]
+        dataset_metadata = LeRobotDatasetMetadata(
+            repo_id=repo_id, root=root
+        )
 
-        self.image_keys = image_keys
+        # get train, val splits
+        fps = dataset_metadata.fps
+        # num_eps = dataset_metadata.total_episodes
+        # val_prop = 0.2                          # validation represents approx 20% of dataset
+        # N_val_eps = int(val_prop * num_eps)
 
-        # Get episode indices for split
-        self.episode_ids = self.dataset.get_split(split)
+        # ep_idxs = list(range(num_eps))
+        # val_ep_idxs = random.sample(ep_idxs, N_val_eps)
+        # train_ep_idxs = list(set(ep_idxs) - set(val_ep_idxs))
 
-        # Build global frame index (episode_id, frame_id)
-        self.index = []
-        for ep_id in self.episode_ids:
-            ep_len = self.dataset.get_episode_length(ep_id)
-            for t in range(ep_len):
-                self.index.append((ep_id, t))
+        # ep_idxs = train_ep_idxs if split=="train" else val_ep_idxs
+
+        # self.dataset = LeRobotDataset(
+        #     repo_id=repo_id,
+        #     root=root,
+        #     tolerance_s=tolerance_s,
+        #     episodes=ep_idxs,
+        #     delta_timestamps={
+        #         "action": [i / fps for i in range(chunk_size)],  # fetch full chunk
+        #     }
+        # )
+
+        def parse_split_range(s: str) -> list[int]:
+            # supports "start:end" (end exclusive)
+            start, end = map(int, s.split(":"))
+            return list(range(start, end))
+
+        info = dataset_metadata.info
+
+        episodes = parse_split_range(info["splits"][split])
+
+        self.dataset = LeRobotDataset(
+            repo_id="surpass/cholecystectomy",
+            root=root,
+            episodes=episodes,
+            tolerance_s=1e-4,
+        )
+        
 
     def __len__(self):
-        return len(self.index)
+        return self.dataset.num_frames
 
     def __getitem__(self, idx):
-        ep_id, t = self.index[idx]
-        frame = self.dataset.get_frame(ep_id, t)
 
-        state = torch.tensor(frame["observation.state"], dtype=torch.float32)
-        action = torch.tensor(frame["action"], dtype=torch.float32)
+        sample = self.dataset[idx]
 
-        images = {}
-        for k in self.image_keys:
-            if k in frame:
-                img = torch.from_numpy(frame[k]).permute(2, 0, 1).float() / 255.0
-                images[k] = img
+        endoscope_image = sample["observation.images.endoscope.left"]
+        wrist_r = sample["observation.images.wrist.right"]
+        wrist_l = sample["observation.images.wrist.left"]
+        state = sample["observation.state"]
+        action_chunk = sample["action"]
+        # action_is_pad = sample["action_is_pad"]
+        instruction_text = sample["instruction.text"]
 
-        return {
-            "obs": state,
-            "action": action,
-            "images": images,
-        }
+        return (
+            endoscope_image,
+            wrist_l,
+            wrist_r,
+            state,
+            action_chunk,
+            # action_is_pad,
+            instruction_text,
+        )
     
 class CholecystectomyACTDataset(Dataset):
     """
@@ -87,11 +122,11 @@ class CholecystectomyACTDataset(Dataset):
         chunk_size: int = 16,
     ):
         super().__init__()
-        valid_splits = {"train", "val"}
+        valid_splits = {"train", "test"}
         assert split in valid_splits, f"Invalid split name, {split}. Must be one of {valid_splits}."
 
-        self.split = split
-        self.chunk_size = chunk_size
+        # self.split = split
+        # self.chunk_size = chunk_size
 
         dataset_metadata = LeRobotDatasetMetadata(
             repo_id=repo_id, root=root
@@ -99,24 +134,43 @@ class CholecystectomyACTDataset(Dataset):
 
         # get train, val splits
         fps = dataset_metadata.fps
-        num_eps = dataset_metadata.total_episodes
-        val_prop = 0.2                          # validation represents approx 20% of dataset
-        N_val_eps = int(val_prop * num_eps)
+        # num_eps = dataset_metadata.total_episodes
+        # val_prop = 0.2                          # validation represents approx 20% of dataset
+        # N_val_eps = int(val_prop * num_eps)
 
-        ep_idxs = list(range(num_eps))
-        val_ep_idxs = random.sample(ep_idxs, N_val_eps)
-        train_ep_idxs = list(set(ep_idxs) - set(val_ep_idxs))
+        # ep_idxs = list(range(num_eps))
+        # val_ep_idxs = random.sample(ep_idxs, N_val_eps)
+        # train_ep_idxs = list(set(ep_idxs) - set(val_ep_idxs))
 
-        ep_idxs = train_ep_idxs if split=="train" else val_ep_idxs
+        # ep_idxs = train_ep_idxs if split=="train" else val_ep_idxs
+
+        # self.dataset = LeRobotDataset(
+        #     repo_id=repo_id,
+        #     root=root,
+        #     tolerance_s=tolerance_s,
+        #     episodes=ep_idxs,
+        #     delta_timestamps={
+        #         "action": [i / fps for i in range(chunk_size)],  # fetch full chunk
+        #     }
+        # )
+
+        def parse_split_range(s: str) -> list[int]:
+            # supports "start:end" (end exclusive)
+            start, end = map(int, s.split(":"))
+            return list(range(start, end))
+
+        info = dataset_metadata.info
+
+        episodes = parse_split_range(info["splits"][split])
 
         self.dataset = LeRobotDataset(
-            repo_id=repo_id,
+            repo_id="surpass/cholecystectomy",
             root=root,
-            tolerance_s=tolerance_s,
-            episodes=ep_idxs,
+            episodes=episodes,
+            tolerance_s=1e-4,
             delta_timestamps={
-                "action": [i / fps for i in range(chunk_size)],  # fetch full chunk
-            }
+                "action": [i / info["fps"] for i in range(chunk_size)],
+            },
         )
         
 
@@ -148,10 +202,38 @@ class CholecystectomyACTDataset(Dataset):
     
 if __name__=="__main__":
     act_dataset = CholecystectomyACTDataset(
-        repo_id="surpass/cholecystectomy_dummy", 
-        root="/home/gbyrd/SURPASS/.hf/lerobot/surpass/cholecystectomy_dummy",
+        repo_id="surpass/cholecystectomy", 
+        root="/home/gbyrd/SURPASS/.hf/lerobot/surpass/cholecystectomy",
         tolerance_s=1e-4,
         split="train"
     )
-    entry = act_dataset[108]
-    pass
+    act_dataset_cs_1 = CholecystectomyACTDataset(
+        repo_id="surpass/cholecystectomy", 
+        root="/home/gbyrd/SURPASS/.hf/lerobot/surpass/cholecystectomy",
+        tolerance_s=1e-4,
+        split="train",
+        chunk_size=1
+    )
+    dataset = CholecystectomyDataset(
+        repo_id="surpass/cholecystectomy", 
+        root="/home/gbyrd/SURPASS/.hf/lerobot/surpass/cholecystectomy",
+        tolerance_s=1e-4,
+        split="train"
+    )
+    
+    N_index = 1000
+
+    start_act = time.time()
+    for i in tqdm(range(N_index)):
+        act_dataset[i]
+    print(f"Time ACT at {N_index} calls: {time.time()-start_act}")
+
+    start_act = time.time()
+    for i in tqdm(range(N_index)):
+        act_dataset_cs_1[i]
+    print(f"Time ACT cs 1 at {N_index} calls: {time.time()-start_act}")
+
+    start = time.time()
+    for i in tqdm(range(N_index)):
+        dataset[i]
+    print(f"Time normal dataset at {N_index} calls: {time.time()-start}")
